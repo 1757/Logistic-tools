629be960dc30883623545c7b1b3449060b8bd0e1
AisuKyubu <huy.le.vu.42@gmail.com>
2016-05-11 01:23:13 +0800

    Do things

--- _parse/cloud/main.js
+++ _parse/cloud/main.js
@@ -1,6 +1,24 @@
-
+/* global Parse */
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
-});
+var regressionCoefficient = require('cloud/regressionParameter.js')
+var _ = require('cloud/lodash.min.js')
+Parse.Cloud.define('compute', function (request, response) {
+  var params = request.params
+
+  var sumCompany = 0
+  var companyCount = 13
+  _.times(companyCount, function (index) {
+    sumCompany += regressionCoefficient[index + 1]
+  })
+  var avgCompany = sumCompany / companyCount
+
+  var regression = 0
+  regression += avgCompany
+  _.each(params, function (value, key) {
+    var coefficient = regressionCoefficient[key]
+    regression += coefficient * value
+  })
+  var output = 1 / (1 / Math.exp(regression) + 1)
+  response.success(output)
+})
