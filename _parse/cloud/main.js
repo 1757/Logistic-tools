56f2ccfbed20b37fb4a8ff4d7d76acfaf4f3c86c
AisuKyubu <huy.le.vu.42@gmail.com>
2016-05-11 01:23:19 +0800

    Do things

--- _parse/cloud/main.js
+++ _parse/cloud/main.js
@@ -1,24 +1,31 @@
 /* global Parse */
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
-var regressionCoefficient = require('cloud/regressionParameter.js')
 var _ = require('cloud/lodash.min.js')
 Parse.Cloud.define('compute', function (request, response) {
-  var params = request.params
+  var query = new Parse.Query("RegressionCoeff")
+  query.descending("updatedAt");
+  query.first().then(function (result) {
+    var regressionCoefficient = result.get('coeff')
+    var params = request.params
 
-  var sumCompany = 0
-  var companyCount = 13
-  _.times(companyCount, function (index) {
-    sumCompany += regressionCoefficient[index + 1]
-  })
-  var avgCompany = sumCompany / companyCount
+    var sumCompany = 0
+    var companyCount = 13
+    _.times(companyCount, function (index) {
+      sumCompany += regressionCoefficient[index + 1]
+    })
+    var avgCompany = sumCompany / companyCount
 
-  var regression = 0
-  regression += avgCompany
-  _.each(params, function (value, key) {
-    var coefficient = regressionCoefficient[key]
-    regression += coefficient * value
+    var regression = 0
+    regression += avgCompany
+    _.each(params, function (value, key) {
+      var coefficient = regressionCoefficient[key]
+      regression += coefficient * value
+    })
+    var output = 1 / (1 / Math.exp(regression) + 1)
+    response.success(output)
+  }, function (error) {
+    response.error("Coefficient not found")
   })
-  var output = 1 / (1 / Math.exp(regression) + 1)
-  response.success(output)
+  
 })
